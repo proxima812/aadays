4339a295f06a6f44bfe4dec348b2b246ef66192b
proxima812 <mrzuc2015@yandex.ru>
2024-05-11 12:33:49 +0500

    first c11mmit

---
+++ src/settings.ts
@@ -0,0 +1,28 @@
+export const config = {
+	site: {
+		base: {
+			url: "https://aadays.vercel.app/",
+		},
+		header: {
+			head: "🔻 Ежедневные размышления АА День за Днем",
+		},
+		index_page: {
+			title: "Размышления АА день за Днем, на сегодня",
+			description: "Ежедневные размышления АА День за Днем 365 дней, на каждый день",
+		},
+		OG: {
+			site_name: "12 steps",
+			author: "samgold",
+			title: "Daily 12 Steps",
+			description: "Daily 12 Steps diaries",
+			locale: "ru",
+			defaultImage: "default-ogImage.png",
+		},
+	},
+	verifications: {
+		yandex: "",
+		google: "",
+		bing: "",
+		// ...
+	},
+}
